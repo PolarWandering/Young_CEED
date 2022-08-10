15f0575e355b17f0962317b5f26d4a2843cdd308
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2022-08-10 18:36:21 +0200

    add progress on summary functions; remove bayes_common_mean with addition of it to pmagpy

--- vgptools/auxiliar.py
+++ vgptools/auxiliar.py
@@ -1,16 +1,15 @@
 import os
 import numpy as np
+import pandas as pd
 from pmagpy import pmag, ipmag
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib.gridspec import GridSpec
+import matplotlib.image as mpimg
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature 
+from cartopy.io.img_tiles import StamenTerrain
 
-
-
-    
-
-    
 
 def get_files_in_directory(path): 
     """
@@ -23,6 +22,7 @@ def get_files_in_directory(path):
     return [os.path.join(path, name) for name in os.listdir(path)
             if (os.path.isfile(os.path.join(path, name)) and (not name.startswith('.')))  ]
 
+
 def cartesian2spherical(v):
     """
     Take an array of lenght 3 correspoingt to a 3-dimensional vector and returns a array of lenght 2
@@ -32,6 +32,7 @@ def cartesian2spherical(v):
     phi = np.arctan2(v[1], v[0])
         
     return [theta, phi]
+
 
 def spherical2cartesian(v):
     """
@@ -44,6 +45,7 @@ def spherical2cartesian(v):
     
     return [x,y,z]
 
+
 def GCD_cartesian(cartesian1, cartesian2):
     
     dot = np.dot(cartesian1, cartesian2)
@@ -52,15 +54,15 @@ def GCD_cartesian(cartesian1, cartesian2):
     
     return gcd
 
+
 def get_k(df_vgps): 
     df_vgps['k'] = np.where(df_vgps['k'].isna(), ((140./df_vgps['alpha95'])**2)/(df_vgps['n']), df_vgps['k'])
     return df_vgps
 
+
 def get_alpha95(df_vgps): 
     df_vgps['alpha95'] = np.where(df_vgps['alpha95'].isna(), 140.0/np.sqrt(df_vgps['n'] * df_vgps['k']),df_vgps['alpha95'])
     return df_vgps
-
-
 
 
 def print_pole_statistics(pole, vgp_mean, vgp_mean_recomputed):
@@ -94,43 +96,95 @@ def test_fishqq(merged):
         plt.show()
 
         
-def reversal_tests(mode1, mode2):
+def statistical_tests(di_mode1, di_mode2, vgps, 
+                      study_number, pole_number, 
+                      save_folder):
     """
     Conduct reversal tests on data of two polarities that have been split using the
-    pmag.separate_directions() function. The function implements the bootstrap 
+    pmag.separate_directions() function. This function implements the bootstrap 
     reversal mean test (Tauxe et al., 1991), the Watson common mean reversal test 
     with classification (McFadden and McElhinny, 1990), and the Bayesian reversal 
-    test (Heslop & Roberts, 2018),
+    test (Heslop & Roberts, 2018). It generates a Dataframe that summarizes the test
+    results.
     
     Parameters
     ----------
-    mode1 : a di_block of directions in one polarity
-    mode2 : a di_block of directions in the opposite polarity
+    di_mode1 : a di_block of directions [dec, inc] in one polarity
+    di_mode2 : a di_block of directions [dec, inc] in the opposite polarity
+    vgps : a di_block of vgp [lon, lat] group in single polarity
+    
     """
     
-    if len(mode1) == 0 or len(mode2) == 0: 
-        print (' ')
-        print (' - Only one polarity; cannot conduct Bootstrapped reversal test')
-    elif len(mode1) < 3 or len(mode2) < 3: 
-        print (' ')
-        print (' - Not enough sites from one (or both) polarity populations to conduct reversal test')
-    else: 
-        flipped2 = ipmag.do_flip(di_block=mode2, unit_vector=False)
-        
-        print('Bootstrap reversal mean test (Tauxe et al., 1991)')
-        print('------------------------------------------------')
-        ipmag.common_mean_bootstrap(mode1, flipped2) 
-        print('')
-        
-        print('Watson common mean reversal test with classification (McFadden and McElhinny, 1990)')
-        print('------------------------------------------------')
-        ipmag.common_mean_watson(mode1, flipped2)
-        print('')
+    stat_test_results = pd.DataFrame(columns = ['result'],
+                                         index=['Bootstrap reversal test', 
+                                                'Parametric reversal test',
+                                                'Bayesian reversal test',
+                                                'Fisher Q-Q test'])
     
-    print('Bayesian reversal test (Heslop & Roberts, 2018)')
+    if len(di_mode1) == 0 or len(di_mode2) == 0: 
+        print (' ')
+        print ('Only one polarity; cannot conduct reversal tests')
+        stat_test_results['result']['Bootstrap reversal test'] = 'Only one polarity'
+        stat_test_results['result']['Parametric reversal test'] = 'Only one polarity'
+        stat_test_results['result']['Bayesian reversal test'] = 'Only one polarity'
+        
+    elif len(di_mode1) < 3 or len(di_mode2) < 3: 
+        print ('')
+        print ('Not enough sites from one (or both) polarity populations to conduct Bootstrap and parametric reversal tests')
+        print ('')
+        stat_test_results['result']['Bootstrap reversal test'] = 'Too few sites for test'
+        stat_test_results['result']['Parametric reversal test'] = 'Too few sites for test'
+        
+        print('Bayesian reversal test (Heslop & Roberts, 2018)')
+        print('------------------------------------------------')
+        B, P, support  = ipmag.common_mean_bayes(np.array(di_mode1), np.array(di_mode2))
+        print('')
+        stat_test_results['result']['Bayesian reversal test'] = support
+    else: 
+        flipped2 = ipmag.do_flip(di_block=di_mode2, unit_vector=False)
+        
+        print('Bootstrap reversal test (Tauxe et al., 1991)')
+        print('------------------------------------------------')
+        bootstrap_result = ipmag.common_mean_bootstrap(di_mode1, flipped2, save=True,
+                                                       save_folder=save_folder,
+                                                       fmt='png') 
+        print('')
+        if bootstrap_result==1:
+            stat_test_results['result']['Bootstrap reversal test'] = 'Pass'
+        if bootstrap_result==0:
+            stat_test_results['result']['Bootstrap reversal test'] = 'Fail'
+        
+        print('Parametric reversal test with classification (McFadden and McElhinny, 1990)')
+        print('------------------------------------------------')
+        result, angle, critical_angle, classification = ipmag.common_mean_watson(di_mode1, flipped2)
+        print('')
+        if result==0:
+            stat_test_results['result']['Parametric reversal test'] = 'Fail (angle ' + str(round(angle,1)) + 'º above ' + str(round(critical_angle,1)) + 'º critical angle)'
+        if result==1:
+            stat_test_results['result']['Parametric reversal test'] = 'Pass (angle ' + str(round(angle,1)) + 'º below ' + str(round(critical_angle,1)) + 'º critical angle); ' + classification + ' classification' 
+    
+        print('Bayesian reversal test (Heslop & Roberts, 2018)')
+        print('------------------------------------------------')
+        B, P, support  = ipmag.common_mean_bayes(np.array(di_mode1), np.array(di_mode2), reversal_test=True)
+        print('')
+        stat_test_results['result']['Bayesian reversal test'] = support
+           
+    print('Fisher distribution Q-Q test (Fisher et al., 1987)')
     print('------------------------------------------------')
-    P = bayes_probability_heslop(mode1, mode2)
-    if P != 9999: print(bayes_support(P))            
+    if len(vgps) <= 10: 
+        print('Not enough sites to conduct Q-Q test')
+        stat_test_results['result']['Fisher Q-Q test'] = 'Too few sites'
+    else:
+        QQ_dict = ipmag.fishqq(di_block = vgps,save=True,
+                               save_folder=save_folder,
+                               fmt='png')
+        plt.show()
+        print(QQ_dict['Test_result'])
+        stat_test_results['result']['Fisher Q-Q test'] = QQ_dict['Test_result']
+    
+    display(stat_test_results)
+
+    return stat_test_results 
 
         
 def invert_polarity(mode1, mode2):
@@ -142,13 +196,182 @@ def invert_polarity(mode1, mode2):
     if mode2.size == 0: 
         merged = mode1
     elif mode1.size == 0: 
-        merged = mode2
+        merged = np.array(ipmag.do_flip(di_block=mode2))[:,:2]
     else:
         flipped2 = np.array(ipmag.do_flip(di_block=mode2))[:,:2]
         #flipped2 = np.delete(np.array(ipmag.do_flip(di_block=mode2)), -1, axis=1)
         merged = np.concatenate((mode1, flipped2))
     
     return merged
+
+
+def scale_bar(ax, length=None, location=(0.5, 0.05), 
+              linewidth=3, zorder=100):
+    """
+    ax is the axes to draw the scalebar on.
+    length is the length of the scalebar in km.
+    location is center of the scalebar in axis coordinates.
+    (ie. 0.5 is the middle of the plot)
+    linewidth is the thickness of the scalebar.
+    """
+    #Get the limits of the axis in lat long
+    llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
+    #Make tmc horizontally centred on the middle of the map,
+    #vertically at scale bar location
+    sbllx = (llx1 + llx0) / 2
+    sblly = lly0 + (lly1 - lly0) * location[1]
+    tmc = ccrs.TransverseMercator(sbllx, sblly)
+    #Get the extent of the plotted area in coordinates in metres
+    x0, x1, y0, y1 = ax.get_extent(tmc)
+    #Turn the specified scalebar location into coordinates in metres
+    sbx = x0 + (x1 - x0) * location[0]
+    sby = y0 + (y1 - y0) * location[1]
+
+    #Calculate a scale bar length if none has been given
+    #(Theres probably a more pythonic way of rounding the number but this works)
+    if not length: 
+        length = (x1 - x0) / 5000 #in km
+        ndim = int(np.floor(np.log10(length))) #number of digits in number
+        length = round(length, -ndim) #round to 1sf
+        #Returns numbers starting with the list
+        def scale_number(x):
+            if str(x)[0] in ['1', '2', '5']: return int(x)        
+            else: return scale_number(x - 10 ** ndim)
+        length = scale_number(length) 
+
+    #Generate the x coordinate for the ends of the scalebar
+    bar_xs = [sbx - length * 500, sbx + length * 500]
+    #Plot the scalebar
+    ax.plot(bar_xs, [sby, sby], transform=tmc, color='k', linewidth=linewidth,zorder=zorder)
+    #Plot the scalebar label
+    ax.text(sbx, sby, str(length) + ' km', transform=tmc,
+            horizontalalignment='center', verticalalignment='bottom',zorder=zorder)
+    
+
+def summary_figure(dataframe, vgp_mode1, vgp_mode2,
+                   reported_pole, vgp_mean, pole_folder):
+    
+    fig = plt.figure(figsize=(8, 12))
+
+    NA_extent = [-120, -65, 15, 65]
+    central_lon = np.mean(NA_extent[:2])
+    central_lat = np.mean(NA_extent[2:]) 
+    NA_proj = ccrs.AlbersEqualArea(central_lon, central_lat)
+
+    lat_buffer = 0.05 * (max(dataframe['slat'])-min(dataframe['slat']))
+    study_extent = [min(dataframe['slon']),max(dataframe['slon']),
+                    min(dataframe['slat'])-lat_buffer,
+                    max(dataframe['slat'])+lat_buffer]
+    study_proj = ccrs.Mercator()
+
+    vgp_proj = ccrs.Orthographic(
+            central_longitude=0, central_latitude=90)
+
+    gs = GridSpec(4, 2, 
+                  width_ratios=[1, 1], 
+                  height_ratios=[1, 1, 0.8, 1.1])
+
+    ax1 = fig.add_subplot(gs[0], projection=NA_proj)
+    ax1.set_extent(NA_extent)
+    ax1.stock_img()
+    ax1.add_feature(cfeature.BORDERS)
+    ax1.gridlines(draw_labels=False)
+    ax1.add_feature(cfeature.LAND)
+    ax1.add_feature(cfeature.COASTLINE)
+    ax1.scatter(x = dataframe['slon'], y = dataframe['slat'], color='r', s=20, 
+                marker='*', transform=ccrs.PlateCarree(),zorder=100,label='site locations')
+    plt.legend()
+    
+    ax2 = fig.add_subplot(gs[1], projection=study_proj)
+    ax2.set_extent(study_extent)
+    
+    tiler = StamenTerrain()
+    mercator = tiler.crs
+    ax2.add_image(tiler, 6)
+
+    gl = ax2.gridlines(draw_labels=True)
+    gl.xlabels_bottom = False
+    gl.ylabels_left = False
+
+    ax2.scatter(x = dataframe['slon'], y = dataframe['slat'], color='r', s=20, 
+                marker='*', transform=ccrs.PlateCarree(),zorder=100,label='site locations')
+    scale_bar(ax2, 10)
+
+    ax3 = fig.add_subplot(gs[2])
+    ipmag.plot_net()
+    ipmag.plot_di(dataframe['dec'].tolist(), dataframe['inc'].tolist(), 
+                        label = "directions")
+    ax3.set_title('site mean directions', 
+                  y=-0.1, backgroundcolor= 'white')
+
+    lat_grid=[-60.0, -30.0, 0.0, 30.0, 60.0]
+    lon_grid=[-180.0, -150.0, -120.0, -90.0, -60.0, -30.0, 0.0, 30.0, 60.0, 90.0, 120.0, 150.0, 180.0]
+    ax4 = fig.add_subplot(gs[3], projection=vgp_proj)
+    ax4.add_feature(cfeature.OCEAN, zorder=0, facecolor='lightblue')
+    ax4.add_feature(cfeature.LAND, zorder=0, facecolor='bisque')
+    ax4.gridlines(xlocs=lon_grid, ylocs=lat_grid, linewidth=1,
+                 color='black', linestyle='dotted')
+
+    mode2_flipped = ipmag.do_flip(di_block=vgp_mode2)
+
+    if len(vgp_mode1)>0:
+        if vgp_mode1[0][1]>0:
+            ipmag.plot_vgp(ax4,di_block=vgp_mode1.tolist(), 
+                           label = "VGPs (normal)", 
+                           marker='s',color='blue')
+            ipmag.plot_vgp(ax4,di_block=mode2_flipped, 
+                           label = "VGPs (reverse)",
+                           marker='d',color='red')
+            plt.legend()
+        else:
+            ipmag.plot_vgp(ax4,di_block=mode2_flipped, 
+                           label = "VGPs (normal)", 
+                           marker='s',color='blue')
+            ipmag.plot_vgp(ax4,di_block=vgp_mode1.tolist(), 
+                           label = "VGPs (reverse)",
+                           marker='d',color='red')
+    elif len(vgp_mode2)>0:
+        if vgp_mode2[0][1]>0: 
+            ipmag.plot_vgp(ax4,di_block=mode2_flipped, 
+                           label = "VGPs (normal)", 
+                           marker='s',color='blue')
+        elif vgp_mode2[0][1]<0:
+            ipmag.plot_vgp(ax4,di_block=mode2_flipped, 
+                           label = "VGPs (reverse)",
+                           marker='d',color='red')
+            
+            
+            
+    if (np.isnan(reported_pole['Plon'].values[0]) == False) & (np.isnan(reported_pole['Plat'].values[0]) == False) & (np.isnan(reported_pole['A95'].values[0]) == False):
+        pole_lat = reported_pole['Plat'].values[0]
+        pole_lon = reported_pole['Plon'].values[0]
+        pole_A95 = reported_pole['A95'].values[0]
+        ipmag.plot_pole(ax4, pole_lon, pole_lat, pole_A95, 
+                        label="reported pole", color='y')
+    if not len(vgp_mean) == 0:
+        ipmag.plot_pole(ax4, vgp_mean['dec'], vgp_mean['inc'], vgp_mean['alpha95'], 
+                             label="recalculated pole", color='red')
+    plt.legend(loc='lower center')    
+    ax4.set_title('virtual geomagnetic poles and mean poles', 
+                  y=-0.1, backgroundcolor= 'white')
+
+    reversal_test_img_path = pole_folder + '/common_mean_bootstrap.png'
+    if os.path.exists(reversal_test_img_path):
+        reversal_test_img = mpimg.imread(reversal_test_img_path)
+        ax5 = fig.add_subplot(gs[2, :])
+        ax5.imshow(reversal_test_img)
+        ax5.axis("off")
+
+    QQ_test_img_path =  pole_folder + '/QQ_mode1.png'
+    if os.path.exists(QQ_test_img_path):
+        QQ_test_img = mpimg.imread(QQ_test_img_path)
+        ax6 = fig.add_subplot(gs[3, :])
+        ax6.imshow(QQ_test_img)
+        ax6.axis("off")
+
+    plt.tight_layout()
+    plt.savefig(pole_folder + '/pole_summary.png',dpi=450)
+
 
 def Plot_VgpsAndSites(df, pole, vgp_mean, mode1, mode2):
     '''
@@ -202,89 +425,6 @@ def Plot_VgpsAndSites(df, pole, vgp_mean, mode1, mode2):
     plt.legend(loc=3, fontsize=12)
     plt.show()
 
-
-
-import scipy as sp #import scipy    
-from scipy.integrate import quad #import 1D quadrature function
-from scipy.integrate import nquad #import ND quadrature function
-import SPD.lib.lib_directional_statistics as lib_direct
-
-
-
-def bayes_probability_heslop(DI1,DI2):
-    '''    
-    Calculate the Bayes probability that the to group of directions
-    are drawn from a single Fisher distributed poopulation following 
-    Heslop and Roberts (2018). 
-    
-    input: two numpy arrays [dec, inc]
-    
-    '''    
-       
-    if DI2.any() & DI1.any():
-        X1=pmag.dir2cart(DI1) #convert normal polarity sites
-        X2=pmag.dir2cart(DI2) 
-        X12=np.concatenate((X1,-X2), axis=0) #pool site directions
-    else:       
-        print(" - Only one polarity; cannot conduct Bayesian Reversal Test")
-        P = 9999
-        return P
-        
-    def log_like(k,N,R):
-        return N*sp.log(k/4/sp.pi)-N*log_sinh(k)+sp.log(4*sp.pi) \
-                +log_sinh(k*R)-sp.log(k*R)
-
-    def log_sinh(k):
-        if k>700:
-            s=k-sp.log(2.0)
-        else:
-            s=sp.log(sp.sinh(k))
-        return s
-
-    def log_prior(k):
-        return np.log(4.0)+2.0*np.log(k)-np.log(np.pi)-2.0*np.log(1.+k**2)
-
-    def integrand1(k,x):
-        R=np.sqrt(np.sum(np.sum(x,axis=0)**2))
-        N=np.size(x,axis=0)
-        val = np.exp(log_like(k,N,R)+log_prior(k))
-        return val
-
-    def integrand2(k,x1,x2):
-        R1=np.sqrt(np.sum(np.sum(x1,axis=0)**2))
-        N1=np.size(x1,axis=0)
-
-        R2=np.sqrt(np.sum(np.sum(x2,axis=0)**2))
-        N2=np.size(x2,axis=0)
-
-        val = np.exp(log_like(k,N1,R1)+log_like(k,N2,R2)+log_prior(k))
-
-        return val        
-    
-    mL1 = quad(integrand1, 0, np.inf, args=(X12))[0] #Bayes factor numerator (Eqn 13)
-    mL2 = quad(integrand2, 0, np.inf, args=(X1,X2))[0] #Bayes factor denominator (Eqn 14)
-    
-    BF0=mL1/mL2
-    P=BF0/(1.+BF0)
-    
-    #return BF0, P
-    return P
-
-def bayes_support(P):
-    '''From Heslop & Roberts (2018)
-     categories of support from the bayesian probability(float)'''
-    
-    support = ['Different means: very strong support','Different means: strong support','Different means: positive support','Ambiguous: weak support',
-              'Common mean: positive support','Common mean: strong support','Common mean: very strong support']
-    
-    if P == 9999: return 
-    if P<0.01: return support[0]
-    if P>=0.01 and P<0.05: return support[1]
-    if P>=0.05 and P<0.25: return support[2]
-    if P>=0.25 and P<0.75: return support[3]
-    if P>=0.75 and P<0.95: return support[4]
-    if P>=0.95 and P<0.99: return support[5]
-    if P>=0.99: return support[6]
 
 def get_site_coordinates(D, I, Plat, Plon):
     '''
